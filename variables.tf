3fac7134455c51c9f1261dec47fa6ead09fb93af
marina-marenkov <97739755+marina-marenkov@users.noreply.github.com>
2022-01-23 15:20:47 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name_prefix" {
+  default       = "rg-tf"
+  description   = "testing"
+}
+
+variable "resource_group_location" {
+  default = "westeurope"
+  description   = "Location of the resource group."
+}
